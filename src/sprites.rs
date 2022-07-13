a7b07fedef25d3f42f354e2fe7399cbd7cdbd775
Eric Wu <eric.dianhao.wu@gmail.com>
2022-07-12 21:11:14 -0400

    remove old sprites

--- src/sprites.rs
+++ src/sprites.rs
@@ -11,14 +11,8 @@ pub static BYTES_TRACKTILE_BLANK: &[u8; 1891] =
 pub static BYTES_TRACKTILE_B: &[u8; 7157] = include_bytes!("../assets/sprites/Tracktile_b.png");
 pub static BYTES_TRACKTILE_H: &[u8; 4624] = include_bytes!("../assets/sprites/Tracktile_h.png");
 pub static BYTES_TRACKTILE_JB: &[u8; 7884] = include_bytes!("../assets/sprites/Tracktile_jb.png");
-pub static BYTES_TRACKTILE_JB_FLIPPED: &[u8; 8323] =
-    include_bytes!("../assets/sprites/Tracktile_jb_flipped.png");
 pub static BYTES_TRACKTILE_JS: &[u8; 6515] = include_bytes!("../assets/sprites/Tracktile_js.png");
-pub static BYTES_TRACKTILE_JS_FLIPPED: &[u8; 6795] =
-    include_bytes!("../assets/sprites/Tracktile_js_flipped.png");
 pub static BYTES_TRACKTILE_M: &[u8; 9902] = include_bytes!("../assets/sprites/Tracktile_m.png");
-pub static BYTES_TRACKTILE_M_FLIPPED: &[u8; 10282] =
-    include_bytes!("../assets/sprites/Tracktile_m_flipped.png");
 pub static BYTES_TRACKTILE_S: &[u8; 2918] = include_bytes!("../assets/sprites/Tracktile_s.png");
 pub static BYTES_TRACKTILE_Z: &[u8; 10917] = include_bytes!("../assets/sprites/Tracktile_z.png");
 pub static BYTES_TRAIN: &[u8; 4943] = include_bytes!("../assets/sprites/Train.png");
@@ -51,11 +45,8 @@ pub struct GameSprites<'a> {
     pub tracktile_b: Texture<'a>,
     pub tracktile_h: Texture<'a>,
     pub tracktile_jb: Texture<'a>,
-    pub tracktile_jb_flipped: Texture<'a>,
     pub tracktile_js: Texture<'a>,
-    pub tracktile_js_flipped: Texture<'a>,
     pub tracktile_m: Texture<'a>,
-    pub tracktile_m_flipped: Texture<'a>,
     pub tracktile_s: Texture<'a>,
     pub tracktile_z: Texture<'a>,
     pub train: Texture<'a>,
@@ -137,11 +128,8 @@ impl<'a> GameSprites<'a> {
             tracktile_b: texture_creator.load_texture_bytes(BYTES_TRACKTILE_B)?,
             tracktile_h: texture_creator.load_texture_bytes(BYTES_TRACKTILE_H)?,
             tracktile_jb: texture_creator.load_texture_bytes(BYTES_TRACKTILE_JB)?,
-            tracktile_jb_flipped: texture_creator.load_texture_bytes(BYTES_TRACKTILE_JB_FLIPPED)?,
             tracktile_js: texture_creator.load_texture_bytes(BYTES_TRACKTILE_JS)?,
-            tracktile_js_flipped: texture_creator.load_texture_bytes(BYTES_TRACKTILE_JS_FLIPPED)?,
             tracktile_m: texture_creator.load_texture_bytes(BYTES_TRACKTILE_M)?,
-            tracktile_m_flipped: texture_creator.load_texture_bytes(BYTES_TRACKTILE_M_FLIPPED)?,
             tracktile_s: texture_creator.load_texture_bytes(BYTES_TRACKTILE_S)?,
             tracktile_z: texture_creator.load_texture_bytes(BYTES_TRACKTILE_Z)?,
             train: texture_creator.load_texture_bytes(BYTES_TRAIN)?,
