887d7c80035a673a7544c511f5efc73fb703bae2
Eric Wu <eric.dianhao.wu@gmail.com>
2022-07-16 17:45:15 -0400

    refactor level loading mechanics

--- src/gameplay.rs
+++ src/gameplay.rs
@@ -49,7 +49,7 @@ impl Gameplay {
             erase_rect,
             speed_slider_space_rect,
             speed_slider_rect,
-            yard: Yard::new(level_manager.get_level("Debug", "Race Condition")),
+            yard: Yard::new(level_manager.get_level( "Purple Parcels")),
             prev_mouse_c: -1,
             prev_mouse_r: -1,
             prev_min_dir: -1,
--- src/levels.rs
+++ src/levels.rs
@@ -26,26 +26,28 @@ pub type City<'a> = (&'a str, Vec<Level<'a>>);
 
 pub struct LevelManager<'a>(Vec<City<'a>>);
 
-fn convert_string_to_color(s: &str) -> Color {
+fn convert_string_to_color(s: &str) -> Option<Color> {
+    // return none if s is invalid input
     match s {
-        "red" => Red,
-        "blue" => Blue,
-        "yellow" => Yellow,
-        "purple" => Purple,
-        "green" => Green,
-        "orange" => Orange,
-        "brown" => Brown,
-        invalid => panic!("invalid color: {}", invalid),
+        "red" => Some(Red),
+        "blue" => Some(Blue),
+        "yellow" => Some(Yellow),
+        "purple" => Some(Purple),
+        "green" => Some(Green),
+        "orange" => Some(Orange),
+        "brown" => Some(Brown),
+        _ => None,
     }
 }
 
-fn convert_string_to_dir(s: &str) -> u8 {
+fn convert_string_to_dir(s: &str) -> Option<u8> {
+    // return none if s was an invalid direction.
     match s {
-        "up" => 0,
-        "right" => 1,
-        "down" => 2,
-        "left" => 3,
-        invalid => panic!("invalid direction: {}", invalid),
+        "up" => Some(0),
+        "right" => Some(1),
+        "down" => Some(2),
+        "left" => Some(3),
+        _ => None,
     }
 }
 
@@ -53,81 +55,142 @@ impl LevelManager<'_> {
     pub fn new() -> LevelManager<'static> {
         let mut lm = LevelManager(vec![]);
         let info_str = str::from_utf8(include_bytes!("../assets/levels.txt")).unwrap();
-        let arr: Vec<&str> = info_str
+        let mut arr = info_str
             .split("\n")
-            .filter(|line| !(line.starts_with("//") || line.is_empty()))
-            .collect();
-        let max_index = arr.len() - 1;
-        let mut index = 0;
-        'outer: loop {
-            assert!(arr[index].starts_with("CITY:"));
-            let city_name = &arr[index][5..];
-            let mut city: City = (city_name, vec![]);
-            index += 1;
+            .enumerate()
+            .map(|(index, line)| (index+1, line))
+            .filter(|(_, line)| !(line.starts_with("//") || line.is_empty()));
+        
+        
+        loop {
+            match arr.next() {
+                None => {
+                    return lm;
+                }
+                Some((line_num, line)) => {
+                    if !line.starts_with("CITY:") {
+                        panic!("expected line {} to start with `CITY:`", line_num)
+                    }
+                    let city_name = &line[5..];
+                    let mut city: City = (city_name, vec![]);
+                    loop {
+                        // load all levels within a city
+                        match arr.next() {
+                            None => {
+                                panic!("unexpected EOF");
+                            }
+                            Some((line_num, line)) => {
+                                if line == "----" {
+                                    break;
+                                }
 
-            loop {
-                // load all levels within a city
-                let fields: Vec<&str> = arr[index].split(":").collect();
-                let level_name = fields[0];
-                let num_stars: u32 = fields[1].parse().unwrap();
-                index += 1;
+                                let fields: Vec<&str> = line.split(":").collect();
+                                if fields.len() != 2 {
+                                    panic!("expected two arguments separated by a `:` at line {}", line_num)
+                                }
+                                let level_name = fields[0];
+                                let num_stars: u32 = fields[1].parse()
+                                    .expect(&format!("expected second argument to be an integer at line {line_num}"));
 
-                let mut level = Level {
-                    level_info: vec![],
-                    name: level_name,
-                    num_stars,
-                };
-                loop {
-                    println!("{}", arr[index]);
-                    let x: u8 = arr[index][2..3].parse().unwrap();
-                    let y: u8 = arr[index][4..5].parse().unwrap();
+                                let level_info = LevelManager::extract_level_from_lines(&mut arr);
 
-                    if arr[index].starts_with("+ ") {
+                                city.1.push(Level {
+                                    level_info,
+                                    name: level_name,
+                                    num_stars,
+                                });
+
+                            }
+                        }
+                    }
+                    lm.0.push(city);
+                }
+            }
+        }
+    }
+
+    fn extract_level_from_lines<'a, I>(arr: &mut I) -> LevelInfo 
+        where I: Iterator<Item = (usize, &'a str)>, 
+    {
+        let mut level_info: LevelInfo = vec![];
+
+        loop {
+            match arr.next() {
+                None => panic!("unexpected EOF"),
+                Some((line_num, line)) => {
+                    if line == "---" {
+                        break;
+                    } 
+
+                    let x: u8 = line[2..3].parse()
+                        .expect(&format!("expected a single-digit integer at the 3rd character of line {line_num}"));
+                    let y: u8 = line[4..5].parse()
+                        .expect(&format!("expected a single-digit integer at the 5th character of line {line_num}"));
+
+                    if line.starts_with("+ ") {
                         // handle a new trainsource
-                        let fields: Vec<&str> = arr[index].split(" ").collect();
-                        let colors: Vec<Color> =
-                            fields[2].split(",").map(convert_string_to_color).collect();
-                        let dir = convert_string_to_dir(fields[3]);
-                        level.level_info.push(PositionedTile {
+                        let fields: Vec<&str> = line.split(" ").collect();
+                        assert!(fields.len() == 4, "wrong number of spaces at line {line_num} (expected 3 spaces)");
+                        let colors: Vec<Color> = fields[2].split(",")
+                            .map(convert_string_to_color)
+                            .map(|o| o.expect(&format!("invalid color at line {line_num}")))
+                            .collect();
+                        let dir = convert_string_to_dir(fields[3])
+                            .expect(&format!("invalid direction in line {line_num}"));
+                        level_info.push(PositionedTile {
                             tile: Tile::Trainsource(Trainsource::new(colors, dir)),
                             x,
                             y,
                         });
-                    } else if arr[index].starts_with("o ") {
+                    } else if line.starts_with("o ") {
                         // handle a new trainsink
-                        let fields: Vec<&str> = arr[index].split(" ").collect();
-                        let colors: Vec<Color> =
-                            fields[2].split(",").map(convert_string_to_color).collect();
-                        let dirs = fields[3].split(",").map(convert_string_to_dir);
+                        let fields: Vec<&str> = line.split(" ").collect();
+                        assert!(fields.len() == 4, "wrong number of spaces at line {line_num} (expected 3 spaces)");
+                        let colors: Vec<Color> = fields[2].split(",")
+                            .map(convert_string_to_color)
+                            .map(|o| o.expect(&format!("invalid color at line {line_num}")))
+                            .collect();
+                        let dirs = fields[3]
+                            .split(",")
+                            .map(convert_string_to_dir)
+                            .map(|o| o.expect(&format!("invalid direction at line {line_num}")));
                         let mut border_state = [false, false, false, false];
                         for dir in dirs {
                             border_state[dir as usize] = true;
                         }
-                        level.level_info.push(PositionedTile {
+                        level_info.push(PositionedTile {
                             tile: Tile::Trainsink(Trainsink::new(colors, border_state)),
                             x,
                             y,
                         });
-                    } else if arr[index].starts_with("* ") {
-                        // handle a new rock
-                        let mut positions = arr[index][2..].split(" ");
+                    } else if line.starts_with("* ") {
+                        // handle a new rock (TODO:)
+                        let mut positions = line[2..].split(" ");
                         while let Some(position) = positions.next() {
-                            let x: u8 = position[0..1].parse().unwrap();
-                            let y: u8 = position[2..3].parse().unwrap();
-                            level.level_info.push(PositionedTile {
+                            let x: u8 = position[0..1].parse()
+                                .expect(&format!("invalid single-digit number on line {line_num}"));
+                            let y: u8 = position[2..3].parse()
+                                .expect(&format!("invalid single-digit number on line {line_num}"));
+                            level_info.push(PositionedTile {
                                 tile: Tile::Rock,
                                 x,
                                 y,
                             });
                         }
-                    } else if arr[index].starts_with("p ") {
+                    } else if line.starts_with("p ") {
                         // handle a new painter
-                        let fields: Vec<&str> = arr[index].split(" ").collect();
-                        let color = convert_string_to_color(fields[2]);
-                        let dirs: Vec<u8> =
-                            fields[3].split(",").map(convert_string_to_dir).collect();
+                        let fields: Vec<&str> = line.split(" ").collect();
+                        assert!(fields.len() == 4, "wrong number of spaces at line {line_num} (expected 3 spaces)");
 
-                        level.level_info.push(PositionedTile {
+                        let color = convert_string_to_color(fields[2]).expect(&format!("invalid color at line {line_num}"));
+                        let dirs: Vec<u8> = fields[3].split(",")
+                            .map(convert_string_to_dir)
+                            .map(|o| o.expect(&format!("invalid direction at line {line_num}")))
+                            .collect();
+                        
+                        assert!(dirs.len() == 2, "expected exactly two directions for painter at line {line_num}");
+
+                        level_info.push(PositionedTile {
                             tile: Tile::Painter(Painter::new(
                                 Connection {
                                     dir1: dirs[0] as u8,
@@ -138,45 +201,27 @@ impl LevelManager<'_> {
                             x,
                             y,
                         });
-                    } else if arr[index].starts_with("s ") {
+                    } else if line.starts_with("s ") {
                         // handle a new splitter
-                        let fields: Vec<&str> = arr[index].split(" ").collect();
-                        let dir = convert_string_to_dir(fields[2]);
-                        level.level_info.push(PositionedTile {
+                        let fields: Vec<&str> = line.split(" ").collect();
+                        assert!(fields.len() == 3, "wrong number of spaces at line {line_num} (expected 2 spaces)");
+                        
+                        let dir = convert_string_to_dir(fields[2]).expect(&format!("invalid direction at line {line_num}"));
+                        level_info.push(PositionedTile {
                             tile: Tile::Splitter(Splitter::new(dir)),
                             x,
                             y,
                         });
                     } else {
-                        panic!("line begins with an invalid character {}", arr[index]);
+                        panic!("line {line_num} begins with an invalid character: {line}");
                     }
-
-                    index += 1;
-                    if arr[index] == "---" {
-                        break;
-                    }
-                }
-                city.1.push(level);
-
-                index += 1;
-                if index > max_index {
-                    lm.0.push(city);
-                    break 'outer;
-                }
-                if arr[index].starts_with("CITY:") {
-                    break;
                 }
             }
-
-            lm.0.push(city);
-
-            // if index > max_index {
-            //     break 'outer;
-            // }
         }
 
-        lm
+        level_info
     }
+
 
     pub fn get_city_names(&self) -> Vec<String> {
         self.0.iter().map(|city| city.0.to_owned()).collect()
@@ -185,13 +230,14 @@ impl LevelManager<'_> {
         let city = self.0.iter().find(|city| city.0 == city_name).unwrap();
         city.1.iter().map(|level| level.name.to_owned()).collect()
     }
-    pub fn get_level(&self, city_name: &str, level_name: &str) -> &LevelInfo {
-        let city = self.0.iter().find(|city| city.0 == city_name).unwrap();
-        &city
-            .1
-            .iter()
-            .find(|level| level.name == level_name)
-            .unwrap()
-            .level_info
+    pub fn get_level(&self, level_name: &str) -> &LevelInfo {
+        for (_, levels) in &self.0 {
+            for level in levels {
+                if level.name == level_name {
+                    return &level.level_info;
+                }
+            }
+        }
+        panic!("no level with name `{level_name}`");
     }
 }
