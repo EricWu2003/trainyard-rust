c809a6347d858eb3f7cd52f7d8a7fa0acdf68bac
Eric Wu <eric.dianhao.wu@gmail.com>
2022-07-28 01:06:48 -0400

    initial macroquad commit

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,36 @@
 pub mod color;
-pub mod connection;
-pub mod edge;
-pub mod gameplay;
-pub mod levels;
+// pub mod connection;
+// pub mod edge;
+// pub mod gameplay;
+// pub mod levels;
 pub mod sprites;
-pub mod tile;
-pub mod yard;
-pub mod utils;
-pub mod particle;
-use crate::gameplay::Gameplay;
-use crate::levels::LevelManager;
+// pub mod tile;
+// pub mod yard;
+// pub mod utils;
+// pub mod particle;
+// use crate::gameplay::Gameplay;
+// use crate::levels::LevelManager;
 use crate::sprites::GameSprites;
-use sdl2::image::{self, InitFlag};
-use sdl2::pixels::Color;
+use macroquad::prelude::*;
 
-use sdl2::rect::Rect;
-use std::time::Duration;
+#[macroquad::main("Trainyard")]
+async fn main() {
+    // let level_manager = LevelManager::new();
 
-fn main() -> Result<(), String> {
-    let level_manager = LevelManager::new();
+    let gs = GameSprites::new().await;
 
-    let sdl_context = sdl2::init()?;
-    let video_subsystem = sdl_context.video()?;
-    let _image_context = image::init(InitFlag::PNG | InitFlag::JPG)?;
-    let window = video_subsystem
-        .window("Trainyard", 700, 900)
-        .position_centered()
-        .resizable()
-        .build()
-        .expect("could not initialize video subsystem");
-
-    let mut canvas = window
-        .into_canvas()
-        .build()
-        .expect("could not make a canvas");
-
-    let texture_creator = canvas.texture_creator();
-    let mut game_sprites = GameSprites::new(&texture_creator)?;
-
-    let mut event_pump = sdl_context.event_pump()?;
-    let yard_rect = Rect::new(14, 10, 672, 672);
-
-    let mut gameplay = Gameplay::new(yard_rect, &level_manager);
+   
+    // let mut gameplay = Gameplay::new(yard_rect, &level_manager);
 
     loop {
-        // Handle events
-        if gameplay.update(&mut event_pump, &game_sprites) {
-            break;
-        }
-
-        // Render
-        // if gameplay.get_state() == YardState::Won {
-        //     println!("won!");
-        // } else if gameplay.get_state() == YardState::Crashed {
-        //     println!("crashed!");
-        // }
-        canvas.set_draw_color(Color::RGB(0, 0, 0));
-        canvas.clear();
-        gameplay.render(&mut canvas, &mut game_sprites)?;
-
-        canvas.present();
-
-        // Time management
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 120));
+        clear_background(LIGHTGRAY);
+        draw_texture_ex(
+           gs.atlas,
+            screen_width() / 2.,
+            screen_height() / 2.,
+            BLUE,
+            DrawTextureParams { dest_size: None, source: Some(gs.train), rotation: 0., flip_x: false, flip_y: false, pivot: None }
+        );
+        next_frame().await;
     }
-
-    Ok(())
 }
--- src/sprites.rs
+++ src/sprites.rs
@@ -1,16 +1,16 @@
+use macroquad::prelude::*;
+use macroquad::audio::Sound;
+use macroquad::audio::load_sound_from_bytes as load_sound;
+use macroquad::audio::play_sound_once;
 use crate::color::Color;
-use sdl2::image::LoadTexture;
-use sdl2::rect::Rect;
-use sdl2::render::{Texture, TextureCreator};
-use sdl2::video::WindowContext;
 
-use soloud::{AudioExt, LoadExt, Soloud};
-use soloud::audio::Wav;
+// use soloud::{AudioExt, LoadExt, Soloud};
+// use soloud::audio::Wav;
 
 
-pub struct GameSprites<'a> {
-    pub atlas: Texture<'a>,
-    pub atlas_color: Texture<'a>,
+pub struct GameSprites {
+    pub atlas: Texture2D,
+    pub atlas_color: Texture2D,
     
     pub tracktile_blank: Rect,
     pub tracktile_b: Rect,
@@ -46,98 +46,94 @@ pub struct GameSprites<'a> {
     pub fire_small: Rect,
     pub star: Rect,
     pub star_bright: Rect,
-    pub sl: Soloud,
-    pub sl_button_press: Wav,
-    pub sl_train_brown: Wav,
-    pub sl_train_yellow: Wav,
-    pub sl_train_red: Wav,
-    pub sl_train_blue: Wav,
-    pub sl_train_purple: Wav,
-    pub sl_train_orange: Wav,
-    pub sl_train_green: Wav,
-    pub sl_splitter: Wav,
-    pub sl_painter: Wav,
-    pub sl_draw_track: Wav,
-    pub sl_switch_track: Wav,
-    pub sl_erase_track: Wav,
-    pub sl_crash: Wav,
-    pub sl_win_level: Wav,
+    pub sl_button_press: Sound,
+    pub sl_train_brown: Sound,
+    pub sl_train_yellow: Sound,
+    pub sl_train_red: Sound,
+    pub sl_train_blue: Sound,
+    pub sl_train_purple: Sound,
+    pub sl_train_orange: Sound,
+    pub sl_train_green: Sound,
+    pub sl_splitter: Sound,
+    pub sl_painter: Sound,
+    pub sl_draw_track: Sound,
+    pub sl_switch_track: Sound,
+    pub sl_erase_track: Sound,
+    pub sl_crash: Sound,
+    pub sl_win_level: Sound,
 }
 
-impl<'a> GameSprites<'a> {
-    pub fn new(
-        texture_creator: &'a TextureCreator<WindowContext>,
-    ) -> Result<GameSprites<'a>, String> {
-        let mut sl_button_press = Wav::default();
-        sl_button_press.load_mem(include_bytes!("../assets/audio/button_press.ogg")).unwrap();
-        let mut sl_train_brown = Wav::default();
-        sl_train_brown.load_mem(include_bytes!("../assets/audio/train_brown.ogg")).unwrap();
-        let mut sl_train_yellow = Wav::default();
-        sl_train_yellow.load_mem(include_bytes!("../assets/audio/train_yellow.ogg")).unwrap();
-        let mut sl_train_red = Wav::default();
-        sl_train_red.load_mem(include_bytes!("../assets/audio/train_red.ogg")).unwrap();
-        let mut sl_train_blue = Wav::default();
-        sl_train_blue.load_mem(include_bytes!("../assets/audio/train_blue.ogg")).unwrap();
-        let mut sl_train_purple = Wav::default();
-        sl_train_purple.load_mem(include_bytes!("../assets/audio/train_purple.ogg")).unwrap();
-        let mut sl_train_orange = Wav::default();
-        sl_train_orange.load_mem(include_bytes!("../assets/audio/train_orange.ogg")).unwrap();
-        let mut sl_train_green = Wav::default();
-        sl_train_green.load_mem(include_bytes!("../assets/audio/train_green.ogg")).unwrap();
-        let mut sl_splitter = Wav::default();
-        sl_splitter.load_mem(include_bytes!("../assets/audio/splitter.ogg")).unwrap();
-        let mut sl_painter = Wav::default();
-        sl_painter.load_mem(include_bytes!("../assets/audio/painter.ogg")).unwrap();
-        let mut sl_draw_track = Wav::default();
-        sl_draw_track.load_mem(include_bytes!("../assets/audio/draw_track.ogg")).unwrap();
-        let mut sl_switch_track = Wav::default();
-        sl_switch_track.load_mem(include_bytes!("../assets/audio/switch_track.ogg")).unwrap();
-        let mut sl_erase_track = Wav::default();
-        sl_erase_track.load_mem(include_bytes!("../assets/audio/erase_track.ogg")).unwrap();
-        let mut sl_crash = Wav::default();
-        sl_crash.load_mem(include_bytes!("../assets/audio/crash.ogg")).unwrap();
-        let mut sl_win_level = Wav::default();
-        sl_win_level.load_mem(include_bytes!("../assets/audio/win_level.ogg")).unwrap();
+impl GameSprites {
+    pub async fn new() -> GameSprites {
+        let sl_button_press = 
+            load_sound(include_bytes!("../assets/audio/button_press.ogg")).await.unwrap();
+        let sl_train_brown = 
+            load_sound(include_bytes!("../assets/audio/train_brown.ogg")).await.unwrap();
+        let sl_train_yellow = 
+            load_sound(include_bytes!("../assets/audio/train_yellow.ogg")).await.unwrap();
+        let sl_train_red = 
+            load_sound(include_bytes!("../assets/audio/train_red.ogg")).await.unwrap();
+        let sl_train_blue = 
+            load_sound(include_bytes!("../assets/audio/train_blue.ogg")).await.unwrap();
+        let sl_train_purple = 
+            load_sound(include_bytes!("../assets/audio/train_purple.ogg")).await.unwrap();
+        let sl_train_orange = 
+            load_sound(include_bytes!("../assets/audio/train_orange.ogg")).await.unwrap();
+        let sl_train_green = 
+            load_sound(include_bytes!("../assets/audio/train_green.ogg")).await.unwrap();
+        let sl_splitter = 
+            load_sound(include_bytes!("../assets/audio/splitter.ogg")).await.unwrap();
+        let sl_painter = 
+            load_sound(include_bytes!("../assets/audio/painter.ogg")).await.unwrap();
+        let sl_draw_track = 
+            load_sound(include_bytes!("../assets/audio/draw_track.ogg")).await.unwrap();
+        let sl_switch_track = 
+            load_sound(include_bytes!("../assets/audio/switch_track.ogg")).await.unwrap();
+        let sl_erase_track = 
+            load_sound(include_bytes!("../assets/audio/erase_track.ogg")).await.unwrap();
+        let sl_crash = 
+            load_sound(include_bytes!("../assets/audio/crash.ogg")).await.unwrap();
+        let sl_win_level = 
+            load_sound(include_bytes!("../assets/audio/win_level.ogg")).await.unwrap();
 
 
-        Ok(GameSprites {
-            atlas: texture_creator.load_texture_bytes(include_bytes!("../assets/atlas.png"))?,
-            atlas_color: texture_creator.load_texture_bytes(include_bytes!("../assets/atlas.png"))?,
-            tracktile_blank: Rect::new(0, 0, 96, 96),
-            tracktile_b: Rect::new(96, 0, 96, 96),
-            tracktile_h: Rect::new(192, 0, 96, 96),
-            tracktile_jb: Rect::new(288, 0, 96, 96),
-            tracktile_js: Rect::new(384, 0, 96, 96),
-            tracktile_m: Rect::new(576, 96, 96, 96),
-            tracktile_s: Rect::new(480, 0, 96, 96),
-            tracktile_z: Rect::new(576, 0, 96, 96),
-            train: Rect::new(104, 96, 32,57),
-            plus_sign: Rect::new(52, 96, 52, 52),
-            circle: Rect::new(0, 96, 52,52),
-            trainsink_entry: Rect::new(1344, 0, 96, 96),
-            source_sink_border: Rect::new(1056, 0, 96,96),
-            trainsource_exit: Rect::new(1440, 0, 96,96),
-            rock: Rect::new(864, 0, 96, 96),
-            painter_bg: Rect::new(672, 0, 96,96),
-            painter_brush: Rect::new(768, 0, 96,96),
-            splitter_bg: Rect::new(1152, 0, 96, 96),
-            splitter: Rect::new(1248, 0, 96, 96),
-            sink_satisfied: Rect::new(960, 0, 96,96),
-            btn_back_to_drawing: Rect::new(0,1700,424,104),
-            btn_erase: Rect::new(0,1908, 208,88),
-            btn_speed: Rect::new(424, 1632, 136, 68),
-            btn_start_trains: Rect::new(0,1804,424,104),
-            btn_status_crashed: Rect::new(424, 1700, 208,168),
-            btn_status_good: Rect::new(424, 1868, 208,168),
-            btn_stop_erase: Rect::new(208,1908, 208,88),
-            space_for_speed_slider: Rect::new(0, 1632, 424, 68),
-            draw_track_arrow: Rect::new(136, 96, 36, 36),
-            smoke: Rect::new(172, 96, 64, 64),
-            fire: Rect::new(236, 96, 64, 64),
-            fire_small: Rect::new(300, 96, 32, 32),
-            star: Rect::new(300, 128, 32, 32),
-            star_bright: Rect::new(332, 96, 32, 32),
-            sl: Soloud::default().unwrap(),
+        GameSprites {
+            atlas: Texture2D::from_file_with_format(include_bytes!("../assets/atlas.png"), None),
+            atlas_color: Texture2D::from_file_with_format(include_bytes!("../assets/atlas.png"), None),
+            tracktile_blank: Rect::new(0., 0., 96., 96.),
+            tracktile_b: Rect::new(96., 0., 96., 96.),
+            tracktile_h: Rect::new(192., 0., 96., 96.),
+            tracktile_jb: Rect::new(288., 0., 96., 96.),
+            tracktile_js: Rect::new(384., 0., 96., 96.),
+            tracktile_m: Rect::new(576., 96., 96., 96.),
+            tracktile_s: Rect::new(480., 0., 96., 96.),
+            tracktile_z: Rect::new(576., 0., 96., 96.),
+            train: Rect::new(104., 96., 32.,57.),
+            plus_sign: Rect::new(52., 96., 52., 52.),
+            circle: Rect::new(0., 96., 52.,52.),
+            trainsink_entry: Rect::new(1344., 0., 96., 96.),
+            source_sink_border: Rect::new(1056., 0., 96.,96.),
+            trainsource_exit: Rect::new(1440., 0., 96.,96.),
+            rock: Rect::new(864., 0., 96., 96.),
+            painter_bg: Rect::new(672., 0., 96.,96.),
+            painter_brush: Rect::new(768., 0., 96.,96.),
+            splitter_bg: Rect::new(1152., 0., 96., 96.),
+            splitter: Rect::new(1248., 0., 96., 96.),
+            sink_satisfied: Rect::new(960., 0., 96.,96.),
+            btn_back_to_drawing: Rect::new(0.,1700.,424.,104.),
+            btn_erase: Rect::new(0.,1908., 208.,88.),
+            btn_speed: Rect::new(424., 1632., 136., 68.),
+            btn_start_trains: Rect::new(0.,1804.,424.,104.),
+            btn_status_crashed: Rect::new(424., 1700., 208.,168.),
+            btn_status_good: Rect::new(424., 1868., 208.,168.),
+            btn_stop_erase: Rect::new(208.,1908., 208.,88.),
+            space_for_speed_slider: Rect::new(0., 1632., 424., 68.),
+            draw_track_arrow: Rect::new(136., 96., 36., 36.),
+            smoke: Rect::new(172., 96., 64., 64.),
+            fire: Rect::new(236., 96., 64., 64.),
+            fire_small: Rect::new(300., 96., 32., 32.),
+            star: Rect::new(300., 128., 32., 32.),
+            star_bright: Rect::new(332., 96., 32., 32.),
             sl_button_press,
             sl_train_brown,
             sl_train_yellow,
@@ -153,36 +149,36 @@ impl<'a> GameSprites<'a> {
             sl_erase_track,
             sl_crash,
             sl_win_level,
-        })
+        }
     }
 
-    pub fn set_color(&mut self, color: Color) {
-        let tint;
-        match color {
-            Color::Brown => tint = (120, 85, 59),
-            Color::Blue => tint = (42, 80, 197),
-            Color::Red => tint = (187, 39, 31),
-            Color::Yellow => tint = (234, 234, 101),
-            Color::Orange => tint = (233, 159, 56),
-            Color::Green => tint = (96, 201, 59),
-            Color::Purple => tint = (161, 32, 197),
-        }
-        let (red, green, blue) = tint;
-        self.atlas_color.set_color_mod(red, green, blue);
-    }
-    pub fn set_alpha(&mut self, alpha: u8) {
-        self.atlas_color.set_alpha_mod(alpha);
-    }
+    // pub fn set_color(&mut self, color: Color) {
+    //     let tint;
+    //     match color {
+    //         Color::Brown => tint = (120, 85, 59),
+    //         Color::Blue => tint = (42, 80, 197),
+    //         Color::Red => tint = (187, 39, 31),
+    //         Color::Yellow => tint = (234, 234, 101),
+    //         Color::Orange => tint = (233, 159, 56),
+    //         Color::Green => tint = (96, 201, 59),
+    //         Color::Purple => tint = (161, 32, 197),
+    //     }
+    //     let (red, green, blue) = tint;
+    //     self.atlas_color.set_color_mod(red, green, blue);
+    // }
+    // pub fn set_alpha(&mut self, alpha: u8) {
+    //     self.atlas_color.set_alpha_mod(alpha);
+    // }
 
     pub fn play_train_sound(&self, color:Color) {
         match color {
-            Color::Brown => self.sl.play(&self.sl_train_brown),
-            Color::Yellow => self.sl.play(&self.sl_train_yellow),
-            Color::Blue => self.sl.play(&self.sl_train_blue),
-            Color::Red => self.sl.play(&self.sl_train_red),
-            Color::Orange => self.sl.play(&self.sl_train_orange),
-            Color::Green => self.sl.play(&self.sl_train_green),
-            Color::Purple => self.sl.play(&self.sl_train_purple),
+            Color::Brown => play_sound_once(self.sl_train_brown),
+            Color::Yellow => play_sound_once(self.sl_train_yellow),
+            Color::Blue => play_sound_once(self.sl_train_blue),
+            Color::Red => play_sound_once(self.sl_train_red),
+            Color::Orange => play_sound_once(self.sl_train_orange),
+            Color::Green => play_sound_once(self.sl_train_green),
+            Color::Purple => play_sound_once(self.sl_train_purple),
         };
     }
 }
